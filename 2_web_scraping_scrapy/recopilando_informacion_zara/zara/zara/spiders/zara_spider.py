b81c373f57b7e69eab6c37722c8c9b35375793ec
Sergio Amorrosta Gotxi <sgotxi500@gmail.com>
2021-02-13 23:56:31 +0100

    Update zara_spider.py
    Problemas de tabulación resueltos

--- 2_web_scraping_scrapy/recopilando_informacion_zara/zara/zara/spiders/zara_spider.py
+++ 2_web_scraping_scrapy/recopilando_informacion_zara/zara/zara/spiders/zara_spider.py
@@ -12,10 +12,10 @@ class ZaraSpider(scrapy.Spider):
     start_urls = [
         'https://www.zara.com/es/es/abrigo-oversize-con-lana-p02268744.html?v1=99425057&v2=1712675',
         'https://www.zara.com/es/es/camiseta-b%C3%A1sica-medium-weight-p00381300.html?v1=86668649&v2=1720409',
-		'https://www.zara.com/es/es/sudadera-capucha-cremalleras-p00761406.html?v1=99422974&v2=1720409',
+	'https://www.zara.com/es/es/sudadera-capucha-cremalleras-p00761406.html?v1=99422974&v2=1720409',
         'https://www.zara.com/es/es/pantal%C3%B3n-chino-carrot-fit-p00706475.html?v1=89361432&v2=1717453',
         'https://www.zara.com/es/es/sandalia-plana-track-p13647710.html?v1=86083745&v2=1471790',
-		'https://www.zara.com/es/es/polo-rayas-p09240419.html?v1=99423481&v2=1720387',
+	'https://www.zara.com/es/es/polo-rayas-p09240419.html?v1=99423481&v2=1720387',
         'https://www.zara.com/es/es/mochila-t%C3%A9cnica-p13201720.html?v1=99425027&v2=1720458'
     ]
     
